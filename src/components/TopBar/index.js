7783ef1b53ba86331c2a9d84d7614ff1c0b49a57
Jussara Soares <jussara.ac.s@hotmail.com>
2020-08-10 23:45:58 -0300

    update topbar

--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 import './styles.css';
 
 function Topbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function onToggle() {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <nav className="topbar">
       <div className="topbar__container">
-        <ul className="topbar__menu">
+        <ul className={`topbar__menu ${isOpen ? 'active' : ''}`}>
           <li className="topbar__logo">
             <a href="/">Rockr Blog</a>
           </li>
@@ -16,8 +24,8 @@ function Topbar() {
           <li className="topbar__item">
             <a href="/contact">Contact</a>
           </li>
-          <li className="topbar__toggle">
-            <a href="#">Toggle</a>
+          <li className="topbar__toggle" onClick={onToggle}>
+            <FontAwesomeIcon icon={faBars} />
           </li>
         </ul>
       </div>
